5b1fdf5b5ee28476f2be63fd7f59924caa351ba1
wangtao <980512523@qq.com>
2023-11-10 18:16:49 +0800

    1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("runner");
+	}
 }
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -3,13 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.component.MyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class IndexController {
+public class IndexController{
 
     @Autowired
     private Environment env;
@@ -20,11 +24,14 @@ public class IndexController {
     @Autowired
     private MyConfig config;
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @GetMapping(value = "/hello")
-    public String hello(){
+    public String hello() {
         System.out.println("hello");
         saveLog();
-        return port+"hello"+ env.getProperty("server.port")+"test"+config.getId()+config.getUsername()+config.getType();
+        return port + "hello" + env.getProperty("server.port") + "test" + config.getId() + config.getUsername() + config.getType();
 
     }
 
@@ -32,4 +39,12 @@ public class IndexController {
     public void saveLog() {
         System.err.println(Thread.currentThread().getName());
     }
+
+
+//    @Bean
+//    public RestTemplate getRestTemplate(){
+//        System.out.println("create resttemplate");
+//        return new RestTemplate();
+//    }
+
 }
